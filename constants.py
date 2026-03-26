33b6b349b48f0cf03de0a823902ffcd3c9c3be38
vt0mii <dev.tomii03@gmail.com>
2026-03-26 15:48:31 -0300

    Modularizacion del codigo, pulido y limpieza de la logica

---
+++ constants.py
@@ -0,0 +1,17 @@
+ASCII_ART = r"""                                     
+    ;/UCQQQ(.           .]QQQJU/^                                 I/Ubah?   ?UULQLJUYYXut{<"    ]Ypobm:                                       
+    '_dh*##?.          -M#*oo]'                                  ,l]pbk{   'IZkoh|iii}cwbaWJ<  lipbq0l                                       
+     +dk&W8o<         lmWM&M&~                                     ?ko*1    ;co*o|     `f##*a?. :Zok0!                                       
+     ~kpjMWBL!       "cBOo8W8i                                     ?a#M}    :xo*o(      "ukbdwl :Z*aJl                                       
+     ?#h?aBB@z.      /%#}#@B@+       +xh8$$Wp('    lfb#@@$8ac<     )8B81    lY&8&f       uokhM< IpBBL!    ijbM@$@&hul   ]fwh8x`(w#B$Baf'     
+     n##\U&MM%L     nW%Z]&8W%f     ]hB&0__UMBBa_  OBB8J-?[h@@%z    QW&8Y    -pkbdQ      1pwmwp] _ka*b{   mBB&X-?|o@@%x  zdBB@h8B#dk%%%8ql    
+     f0m| UkQZa1   ;wdm_]odwh|    -#8m,    |W#&Q,]d&8*<   t#%8L    Xqwbf    +CUXCX     1wqdap;  ~QYJL~  }d*Mh>   jW%%U   [b8WM0>   -qbww<    
+     ]CU- :nQzXx! .|YU- >JzcJ_   ,nwd*ao*#WWaqwCI -jn)  .`~Owqr    (zvY]    lnUXYZLYUZOC0br_    lXYUrI   [rr1  .`_pbht   <YZ0Y`     tUcz;    
+     !j/I  ,rxfc? Ixx<  :rffv;   ljf(>>>>>>>>>>!    'I-/ujxrff-    >jfx!    "}jfr+"i>>i"        "\jj]"     'l-rznvuxx_   :1tf}.     _//t^    
+     :/("   ,tt//,-f?   ^t//x"   ,///:            ^</\};` "(/ji    ;//f:    '_t/f!              `)/t+`   "_f/};' ,)(tl   ^]tf?.     !t/|'    
+     It|:    >uff|x|:   ,fffx:   ,1jf)"       '  Itfx<    Itfr<    !t/jI    ^]jfr<              ^(tt-`  ltfx>    Iffr>   ,}tf[.     <tt\^    
+     ~\|>    '}jt/j+    Ijttri    +f\\\_"   !]/- [\(/+   ,)jtf-    -/\f>    ;)ttf?              ,///}"  {||t<   ;(ftj+   l(/t1'     ]/\t,    
+    ,-//?I;`  ;(xx]`   "<rffr+"   '+f/\|\\|\|/}" +f//t()(tj1rt{, 'l{ttf-:  '!ffff[:"           ,<f/t\i` +f//t()(tr1jt[,  it/t(;,  ;l{t/f>`   
+   I{ffftft_'  :Yn^   I)jrxxjj1"    "~xpqqqQ}l.   I[Lbbpc-:lrjj-.!\jjrjj{; _jjxxrjf[,          ?jjrjjf~  l}0bdpu-"!rjj_ !/jjjjf-  _fjjrjti   
+"""
+MENU_OPTIONS = [ "Ya tengo una cuenta :)", "No tengo cuenta :(" ]
