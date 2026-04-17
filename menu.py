e026ebd8fdf4f4b8432efc235af4e461ba29129a
Azicari3 <augustozicari@gmail.com>
2026-04-17 16:30:08 -0300

    Se agrega la función para añadir recetas

--- menu.py
+++ menu.py
@@ -156,12 +156,14 @@ def recetas_menu(user_id):
         if selected == 0:
             flag = False
         elif selected == 1:
-            print("Función para añadir receta")
             print("--------------AÑADIR RECETAS-------------") 
             print()
             title = (input("Porfavor Ingrese el nombre de la receta "))
+            while not v.validate_alphabetic(title):
+                title = input("Ingrese un nombre con letras unicamente")
+
             print()
-            instructions = str(input("Porfavor Ingrese el nombre de la receta "))
+            instructions = input("Porfavor Ingrese el nombre de la receta ")
             f.add_recipe(user_id,title,instructions)
         elif selected == 2:
             print("Función para quitar receta")
